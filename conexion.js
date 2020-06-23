25eee418f439f70849e0817842b79c0fa97c23bc
FelipeA0 <gonzalez.aegf@gmail.com>
2020-06-23 03:49:20 -0500

    nueva base mysql

--- conexion.js
+++ conexion.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 module.exports = mysql.createConnection({
- host: "localhost",
- user: "root",
- password: "",
- database: "apitest"
+ host: "sql9.freemysqlhosting.net",
+ user: "sql9350265",
+ password: "nPrDM6CWxT",
+ database: "sql9350265"
 });
